66785182fcdf35cd992a6bd0d4cd152186f27d85
YoannCollet <yoann.collet11@gmail.com>
2017-12-07 12:44:58 +0100

    Start dev model

--- src/yolocorp/estock/model/Produit.java
+++ src/yolocorp/estock/model/Produit.java
@@ -1,5 +1,28 @@
 package yolocorp.estock.model;
 
 public class Produit {
-
+	
+	private int quantiteStock;
+	private String nom;
+	private float prixUnitaireHT;
+	private float tauxTVA = 0.2f;
+	
+	public Produit(String nom, float prixUnitaireHT, int quantiteStock) {
+		this.nom = nom;
+		this.prixUnitaireHT = prixUnitaireHT;
+		this.quantiteStock = quantiteStock;
+	}
+	
+	public boolean ajouter(int quantiteAchetee){
+		
+		return true;
+	}
+	
+	public boolean enlever(int quantiteVendue){
+		return true;
+	}
+	
+	public float getPrixUnitaireHT() {
+		return this.prixUnitaireHT;
+	}
 }
