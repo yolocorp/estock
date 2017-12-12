b003b0b2e60dac8e9c58da399a3fb7d389db61ce
Yoann Collet <yoann.collet11@gmail.com>
2017-12-12 23:37:40 +0100

    implementation of Produit

--- src/yolocorp/estock/model/Produit.java
+++ src/yolocorp/estock/model/Produit.java
@@ -14,15 +14,37 @@ public class Produit {
 	}
 	
 	public boolean ajouter(int quantiteAchetee){
-		
+		quantiteStock = quantiteStock + quantiteAchetee;
 		return true;
 	}
 	
 	public boolean enlever(int quantiteVendue){
+		quantiteStock = quantiteStock - quantiteVendue;
 		return true;
 	}
 	
 	public float getPrixUnitaireHT() {
 		return this.prixUnitaireHT;
+	}
+	
+	public float getPrixUnitaireTTC(){
+		return this.prixUnitaireHT * tauxTVA;
+	}
+	
+	public float getPrixStockTTC(){
+		return quantiteStock * getPrixUnitaireTTC();
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int getQuantite() {
+		return quantiteStock;
+	}
+	
+	public String toString(){
+		return "Nom : " + getNom() + ", Quantité : " + quantiteStock + ", Prix Unitaire HT : " + getPrixUnitaireHT()
+				+ ", Prix Unitaire TTC : " + getPrixUnitaireTTC() + ", Prix Stock TTC : " + getPrixStockTTC();
 	}
 }
