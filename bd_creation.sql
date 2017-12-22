e91ef9dcfa167474a8f91c8af2986a2f68a7afa5
Yoann Collet <yoann.collet11@gmail.com>
2017-12-22 12:39:36 +0100

    Creation ProduitDAO & update bd_creation.sql

--- bd_creation.sql
+++ bd_creation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE produit (
 	produit_id number constraint produit_id_nn not null,
 	produit_nom nvarchar2 constraint produit_nom_nn not null,
-	produit_prix_ht constraint produit_prix_ht_nn not null,
-	produit_taux_tva constraint produit_taux_tva not null
+	produit_prix_ht double constraint produit_prix_ht_nn not null,
+	produit_quantite_stock int constraint produit_quantite_stock_nn not null
 );
---
+++ src/yolocorp/estock/model/ProduitDAO.java
@@ -0,0 +1,71 @@
+package yolocorp.estock.model;
+
+import java.sql.*;
+
+public class ProduitDAO {
+	
+	private Connection cn;
+	private Statement st;
+	
+	public ProduitDAO() {
+	
+		Connection cn;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			cn = DriverManager.getConnection("jdbc:oracle:thin:@162.38.222.149:1521:iut", "collety", "1108009694N");
+			st = cn.createStatement();
+		} catch (SQLException e ) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public void createProduit(String nom, double prix, int quantite ) {
+		try {
+			CallableStatement cst = cn.prepareCall("{call ajouterProduit(?,?,?)}");
+			cst.setString(1, nom);
+			cst.setDouble(2, prix);
+			cst.setInt(3, quantite);
+			cst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readProduit(String nom) {
+	
+		try {
+			PreparedStatement pst = cn.prepareStatement("SELECT * FROM PRODUITS WHERE produit_nom = ?");
+			pst.setString(1, nom);
+			pst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void updateProduit(String nom, double prix, int quantite) {
+		try {
+			CallableStatement cst = cn.prepareCall("{call modifierProduit(?,?,?)}");
+			cst.setString(1, nom);
+			cst.setDouble(2, prix);
+			cst.setInt(3, quantite);
+			cst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteProduit(String nom) {
+		try {
+			CallableStatement cst = cn.prepareCall("{call ajouterProduit(?)}");
+			cst.setString(1, nom);
+			cst.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
