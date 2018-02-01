9ff66574f8dcfddeb2c399e385af45317424ccab
louis <louis-rogeon@outlook.fr>
2018-02-01 23:54:23 +0100

    Push part 3

--- bd_creation.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE produit (
-	produit_id number constraint produit_id_nn not null,
-	produit_nom nvarchar2 constraint produit_nom_nn not null,
-	produit_prix_ht double constraint produit_prix_ht_nn not null,
-	produit_quantite_stock int constraint produit_quantite_stock_nn not null
-);
